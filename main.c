3fdb93e83f7404e3534789837c25feeae65e9aa1
Joshua Aitken <ajo107@uclive.ac.nz>
2017-08-11 01:33:16 +1245

    Take values from the commandline instead

--- main.c
+++ main.c
@@ -9,6 +9,15 @@ typedef struct equ {
     int val;
 } EA;
 
+void argHandler(int* args)
+{
+    if (args[0] >= args[1]) {
+        int tmp = args[0];
+        args[0] = args[1];
+        args[1] = tmp;
+    };
+}
+
 int euclidean(int* a, int* b, int* r, int* q, int* val)
 {
     while (*r != 0){
@@ -28,11 +37,17 @@ int euclidean(int* a, int* b, int* r, int* q, int* val)
     return *val;
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    int arg1 = atoi(argv[1]);
+    int arg2 = atoi(argv[2]);
+
+    int args[2] = {arg1, arg2};
+    argHandler(args);
+    
     EA gcd;
-    gcd.a = 239238;
-    gcd.b = 372843;
+    gcd.a = args[0];
+    gcd.b = args[1];
     gcd.r = -1;
     gcd.q = 0;
     gcd.val = 0;
