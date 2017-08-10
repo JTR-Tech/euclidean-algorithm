e10c32b426d8c399907b03336406eb9582d277a1
Joshua Aitken <ajo107@uclive.ac.nz>
2017-08-11 01:43:29 +1245

    Improve variable naming, add comments

--- main.c
+++ main.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 
 typedef struct equ {
-    int a;
-    int b;
-    int r;
-    int q;
+    int smallest;
+    int largest;
+    int remainder;
+    int quotient;
     int val;
 } EA;
 
 void argHandler(int* args)
 {
+    //sort our cmdline args smallest to largest
     if (args[0] >= args[1]) {
         int tmp = args[0];
         args[0] = args[1];
@@ -18,18 +19,19 @@ void argHandler(int* args)
     };
 }
 
-int euclidean(int* a, int* b, int* r, int* q, int* val)
+int euclidean(int* smallest, int* largest, int* remainder, int* quotient, int* val)
 {
-    while (*r != 0){
-        *r = *b % *a;
-        if (*r == 0){
-            *val = *a;
+    //Recursive function implementing the Euclidean Algorithm to find the GCD
+    while (*remainder != 0){
+        *remainder = *largest % *smallest;
+        if (*remainder == 0){
+            *val = *smallest;
             return *val;
         } else {
-            *q = *b / *a;
-            *b = *a;
-            *a = *r;
-            euclidean(a, b, r, q, val);
+            *quotient = *largest / *smallest;
+            *largest = *smallest;
+            *smallest = *remainder;
+            euclidean(smallest, largest, remainder, quotient, val);
         };
 
     };
@@ -39,19 +41,19 @@ int euclidean(int* a, int* b, int* r, int* q, int* val)
 
 int main(int argc, char *argv[])
 {
+    //Convert command line arguments to ints, then sort smallest to largest
     int arg1 = atoi(argv[1]);
     int arg2 = atoi(argv[2]);
-
     int args[2] = {arg1, arg2};
     argHandler(args);
-    
+
     EA gcd;
-    gcd.a = args[0];
-    gcd.b = args[1];
-    gcd.r = -1;
-    gcd.q = 0;
+    gcd.smallest = args[0];
+    gcd.largest = args[1];
+    gcd.remainder = -1;
+    gcd.quotient = 0;
     gcd.val = 0;
 
-    printf("GCD: %d\n", euclidean(&gcd.a, &gcd.b, &gcd.r, &gcd.q, &gcd.val));
+    printf("GCD: %d\n", euclidean(&gcd.smallest, &gcd.largest, &gcd.remainder, &gcd.quotient, &gcd.val));
     return EXIT_SUCCESS;
 }
